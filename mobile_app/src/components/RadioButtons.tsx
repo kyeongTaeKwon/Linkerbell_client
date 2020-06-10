b7be2516114e1e33cef4810724aba295cdb53f4b
Elena Lee <jelee.elena@gmail.com>
2020-06-10 15:30:56 +0900

    Add setValue to RadioButton so that it changes gender value

--- mobile_app/src/components/RadioButtons.tsx
+++ mobile_app/src/components/RadioButtons.tsx
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { Layout, Radio, RadioGroup } from "@ui-kitten/components";
+import { UserDetailValue } from "../screens/userDetail";
 
-const RadioButtons = (): JSX.Element => {
+type RadioProps = {
+  value: UserDetailValue;
+  setValue: React.Dispatch<React.SetStateAction<UserDetailValue>>;
+};
+
+const RadioButtons = ({ value, setValue }: RadioProps): JSX.Element => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   return (
     <Layout style={styles.radioContainer} level="1">
       <RadioGroup
         selectedIndex={selectedIndex}
-        onChange={(index) => setSelectedIndex(index)}
+        onChange={(index) => {
+          setSelectedIndex(index);
+          setValue({ ...value, gender: index });
+        }}
       >
+        <Radio style={styles.radio}>해당없음</Radio>
         <Radio style={styles.radio}>여성</Radio>
         <Radio style={styles.radio}>남성</Radio>
-        <Radio style={styles.radio}>해당없음</Radio>
       </RadioGroup>
     </Layout>
   );
