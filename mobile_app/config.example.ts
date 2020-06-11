7950c8769f342ddb2ae22d523a680d3f84a68139
Elena Lee <jelee.elena@gmail.com>
2020-06-11 22:50:37 +0900

    Add config.example.ts

--- mobile_app/config.example.ts
+++ mobile_app/config.example.ts
@@ -1,1 +1,16 @@
-
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+export const googleKey = {
+  AndroidKey: "",
+  iosKey: "",
+};
+export const facebookKey = {
+  key: "",
+};
