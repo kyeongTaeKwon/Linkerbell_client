9f3d75a33fe4d46644cd733ec3c2bd67c7558b28
Elena Lee <jelee.elena@gmail.com>
2020-06-13 21:27:31 +0900

    Add onPress function to FlatList to route to list page with category_id

--- mobile_app/src/screens/home.tsx
+++ mobile_app/src/screens/home.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
-// import { LoginValue } from "../models/LoginTypes";
+import { StyleSheet, View, FlatList } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { style } from "../styles/HomeStyles/HStyleIndex";
 import { ShortBar } from "../styles/ShortBar";
-const { UpperText, TitleText, CategoryText, HContainer } = style;
+const { UpperText, TitleText, HContainer } = style;
 import useLinkData from "../hooks/useLinkData";
 import Item from "../components/Category";
 // import FakeData from "../core/services/fakeData";
@@ -21,14 +20,14 @@ const Home = ({
 
   const { onHome, categories } = useLinkData();
 
+  const onPress = (category_id: number) => {
+    navigation.navigate("List", { category_id });
+  };
+
   const getCategoryList = async () => {
     const categories = await fetchCategoryRequest();
     onHome(categories);
   };
-
-  // const onPress = () => {
-  //   navigation.navigate("list");
-  // };
 
   useEffect(() => {
     getCategoryList();
@@ -39,20 +38,16 @@ const Home = ({
     console.log(categories);
   }, [categories]);
 
-  // onPress={() => navigation.navigate("list")} // 리스트페이지로 네비게이트
-
   return (
     <HContainer>
       <UpperText>전체 글 보기</UpperText>
       <ShortBar />
-      <TitleText onPress={() => navigation.navigate("List")}>
-        카테고리
-      </TitleText>
+      <TitleText>카테고리</TitleText>
       <View style={styles.outerContainer}>
         <FlatList
           keyExtractor={(item) => item.category_id.toString()}
           data={data}
-          renderItem={({ item }) => <Item item={item} /*onPress={onPress}*/ />}
+          renderItem={({ item }) => <Item item={item} onPress={onPress} />}
         />
       </View>
     </HContainer>
