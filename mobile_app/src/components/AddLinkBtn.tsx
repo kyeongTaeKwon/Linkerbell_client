48211fe8e85485b5233f79959ba22934c1593723
Elena Lee <jelee.elena@gmail.com>
2020-06-16 16:40:14 +0900

    Fix touchable area maxWidth

--- mobile_app/src/components/AddLinkBtn.tsx
+++ mobile_app/src/components/AddLinkBtn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 
 type Props = {
@@ -8,14 +8,6 @@ type Props = {
 };
 const AddLinkBtn = ({ onPress }: Props): JSX.Element => {
   return (
-    // <MaterialIcons
-    //   name="add-box"
-    //   size={75}
-    //   color="#FFD93B"
-    //   style={styles.addButton}
-    //   onPress={onPress}
-    // />
-    //! switch
     <TouchableOpacity style={styles.button} onPress={onPress}>
       <MaterialCommunityIcons name="plus" size={43} color="white" />
     </TouchableOpacity>
@@ -25,20 +17,6 @@ const AddLinkBtn = ({ onPress }: Props): JSX.Element => {
 export default AddLinkBtn;
 
 const styles = StyleSheet.create({
-  addButton: {
-    alignSelf: "flex-end",
-    position: "absolute",
-    marginTop: 680,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.08,
-    shadowRadius: 16,
-    elevation: 40,
-    borderRadius: 5,
-  },
   button: {
     alignSelf: "flex-end",
     position: "absolute",
