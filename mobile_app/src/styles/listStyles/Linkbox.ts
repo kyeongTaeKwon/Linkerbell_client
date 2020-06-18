fa673a7e9e1ea07be251ccbb4a87652b6eae493a
kwon <sjbby100@naver.com>
2020-06-18 16:43:01 +0900

    Add Link panResponder

--- mobile_app/src/styles/listStyles/Linkbox.ts
+++ mobile_app/src/styles/listStyles/Linkbox.ts
@@ -3,15 +3,15 @@ import styled from "styled-components/native";
 export const LinkBox = styled.View`
   /* flex: 1; */
   flex-direction: row;
-  width: 375px;
+  /* width: 375px; */
   /* flex-grow: 1; */
-  /* height: 110px; */
+  height: 100px;
   margin-bottom: 40px;
   /* padding-right: 200px;
    */
   /* padding-bottom: 20px;
   border: 1px; */
   background-color: #fefefe;
-  border-radius: 12px;
+  /* border-radius: 12px; */
   /* border-bottom: 1px; */
 `;
