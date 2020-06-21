0cedfabf2b4e88406451d02d5dd7cf1027471d39
Elena Lee <jelee.elena@gmail.com>
2020-06-22 02:16:59 +0900

    Add fake email address to myPage

---
+++ mobile_app/src/styles/MypageStyles./EmailView.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components/native";
+
+export const EmailView = styled.View`
+  margin-left: 36px;
+  margin-top: 20px;
+  margin-right: 36px;
+`;
+
+export const EmailText = styled.Text`
+  font-family: "NBold";
+  font-size: 16px;
+  color: #000000;
+  line-height: ${18 * 1.6}px;
+`;
+
+export const Email = styled.Text`
+  font-family: "NMedium";
+  font-size: 16px;
+  margin-left: 10px;
+  /*line-height: ${12 * 3}px;*/
+`;
+
+export const EmailContainer = styled.View`
+  background-color: #fff;
+  justify-content: center;
+  elevation: 5;
+  border-radius: 4px;
+`;
+// shadowColor: "#000",
+// shadowOffset: {
+//   width: 0,
+//   height: 3,
+// },
+// shadowOpacity: 0.08,
+// shadowRadius: 16,
