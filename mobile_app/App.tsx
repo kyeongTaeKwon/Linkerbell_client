57e087c8cf4d1e6de3fcbc0b79667496be832b8d
kwon <sjbby100@naver.com>
2020-06-25 14:28:45 +0900

    Eject project

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./src/store/index";
+import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import { Routes } from "./src/routes";
@@ -8,12 +9,11 @@ import * as eva from "@eva-design/eva";
 import { ApplicationProvider } from "@ui-kitten/components";
 import firebase from "firebase";
 import { firebaseConfig } from "./config";
-
+import * as SplashScreen from "expo-splash-screen";
 firebase.initializeApp(firebaseConfig);
 
 export default function App(): JSX.Element {
   const [isReady, setReady] = useState(false);
-
   const getFonts = () => {
     return Font.loadAsync({
       NMedium: require("./assets/fonts/NotoSansKR-Medium.otf"),
@@ -21,16 +21,41 @@ export default function App(): JSX.Element {
       NRegular: require("./assets/fonts/NotoSansKR-Regular.otf"),
     });
   };
+  useEffect(() => {
+    async function asyncTasks() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+      await loadResourcesAsync();
+      setReady(true);
+    }
 
-  if (isReady) {
-    return (
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Provider store={store}>
-          <Routes />
-        </Provider>
-      </ApplicationProvider>
-    );
-  } else {
-    return <AppLoading startAsync={getFonts} onFinish={() => setReady(true)} />;
+    asyncTasks();
+  }, []);
+  async function loadResourcesAsync() {
+    await Promise.all([
+      // Asset.loadAsync([
+      //   require("./assets/images/logo.png"), // Load your resources here (if any)
+      // ]),
+      Font.loadAsync({
+        NMedium: require("./assets/fonts/NotoSansKR-Medium.otf"),
+        NBold: require("./assets/fonts/NotoSansKR-Bold.otf"),
+        NRegular: require("./assets/fonts/NotoSansKR-Regular.otf"),
+      }),
+    ]);
+    await SplashScreen.hideAsync();
   }
+  // if (isReady) {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </ApplicationProvider>
+  );
+  // } else {
+  // return <AppLoading startAsync={getFonts} onFinish={() => setReady(true)} />;
+  // }
 }
